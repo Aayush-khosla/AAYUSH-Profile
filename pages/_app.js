97f9a0c70b4be14233593c3228fc54c409e3ed3e
Suryam1 <suryam1260.be21@chitkarauniversity.edu.in>
2022-09-15 12:42:23 +0530

    Added components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import NavBar from '../components/NavBar'
+import Footer from '../Components/Footer'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <NavBar />
+    <Component {...pageProps} />
+    <Footer />
+  </>
 }
 
 export default MyApp
